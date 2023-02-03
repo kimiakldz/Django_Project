0f38ff384e9ac34e0be56ffd908e4247482a8c12
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-03 13:26:51 +0330

    006_develope: Customer model created

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
+
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20, widget=forms.PasswordInput)
+
+
