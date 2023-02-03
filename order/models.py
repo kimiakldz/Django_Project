c463af03a1d31e872a483d2472d7251d985a971b
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-03 13:45:21 +0330

    010_develope: Discount_code model added

--- order/models.py
+++ order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Discount_code(models.Model):
+    percent = models.FloatField()
+    price = models.FloatField()
+    max = models.FloatField()
+    expire_date = models.DateField()
+    one_time = models.BooleanField()
