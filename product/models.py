3dd485edf3978eba5c23532cd566a749840acc96
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-03 14:09:53 +0330

    013_develope: Product model added

--- product/models.py
+++ product/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Product(models.Model):
-    pass
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.CharField(max_length=250)
+    Stock = models.IntegerField()
+    price_dis = models.FloatField()
+    percent_dis = models.FloatField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
