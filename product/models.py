3daa667fde99b8439d263d33f659bf335f59d4a1
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-03 14:08:02 +0330

    012_develope: Category model added

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
+
