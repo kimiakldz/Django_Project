13bd9a2f016dad7fb2bc03b088b659306d4371a9
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-09 16:49:38 +0330

    016_develope: customers app deleted

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django import forms
+
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    username = models.CharField(max_length=20)
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+
+
+class Address(models.Model):
+    province = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    alley = models.CharField(max_length=50)
+    num = models.CharField(max_length=5)
+    postal_code = models.CharField(max_length=10)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+
