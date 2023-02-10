4a5d0523d014bcfad7350f841b8c9cfa36931489
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-10 09:59:08 +0330

    024_develope: order models' fields completed

--- order/models.py
+++ order/models.py
@@ -5,22 +5,36 @@ from product.models import Product
 
 # Create your models here.
 class DiscountCode(models.Model):
-    value = models.FloatField()
-    type = models.FloatField()
-    max = models.FloatField()
-    expire_date = models.DateField()
-    one_time = models.BooleanField()
+    value = models.DecimalField(max_digits=20, decimal_places=2)
+    type = models.TextChoices('DiscountType', 'Price Percent')
+    max = models.DecimalField(max_digits=20, decimal_places=2)
+    expire_date = models.DateField(null=True, blank=True)
+    one_time = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.type}: {self.value}"
 
 
 class Order(models.Model):
-    total_price = models.FloatField()
-    date = models.DateTimeField()
-    status = models.TextChoices('OrderStatus', 'Ordered InProgress Sent Delivered')
+    ORDERED = 'OR'
+    INPROGRESS = 'IP'
+    SENT = 'SE'
+    DELIVERED = 'DE'
+    Order_Status = [
+        (ORDERED, 'Ordered'),
+        (INPROGRESS, 'InProgress'),
+        (SENT, 'Sent'),
+        (DELIVERED, 'Delivered'),
+    ]
+    status = models.CharField(
+        max_length=2,
+        choices=Order_Status,
+        default=ORDERED,
+    )
+    total_price = models.DecimalField(max_digits=20, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    code_id = models.ForeignKey(DiscountCode, on_delete=models.CASCADE)
+    code_id = models.ForeignKey(DiscountCode, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.date}ـ{self.total_price}$"
