031735a43ffa4861dfac5c756b6bb67a7c6be1f0
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-10 09:37:34 +0330

    023_develope: product models' fields completed

--- product/models.py
+++ product/models.py
@@ -6,16 +6,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Discount(models.Model):
-    value = models.FloatField()
-    type = models.FloatField()
-    max = models.FloatField()
+    value = models.DecimalField(max_digits=20, decimal_places=2)
+    type = models.TextChoices('DiscountType', 'Price Percent')
+    max = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
         return f"{self.type}: {self.value}"
@@ -23,12 +23,12 @@ class Discount(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    price = models.FloatField()
-    description = models.TextField()
-    image = models.ImageField(upload_to="")
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="static/image", null=True, blank=True)
     Stock = models.IntegerField()
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    discount_id = models.ForeignKey(Discount, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    discount_id = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}: {self.price}$"
