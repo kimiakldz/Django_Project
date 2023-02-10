2b4ef02907c7ae4a6d3a44fa7bd46cca3a43b641
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-10 09:10:25 +0330

    022_develope: srt method for product models added

--- product/models.py
+++ product/models.py
@@ -8,11 +8,17 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Discount(models.Model):
     value = models.FloatField()
     type = models.FloatField()
     max = models.FloatField()
+
+    def __str__(self):
+        return f"{self.type}: {self.value}"
 
 
 class Product(models.Model):
@@ -23,3 +29,6 @@ class Product(models.Model):
     Stock = models.IntegerField()
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     discount_id = models.ForeignKey(Discount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}: {self.price}$"
