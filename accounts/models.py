ae853ae2c9ff4aa566c19c54575be1c3c0dd2b5a
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-10 08:53:50 +0330

    020_develope: srt method for accounts models added

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,9 @@ class Customer(models.Model):
     username = models.CharField(max_length=20)
     password = forms.CharField(max_length=20, widget=forms.PasswordInput)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Address(models.Model):
     province = models.CharField(max_length=50)
@@ -20,6 +23,9 @@ class Address(models.Model):
     num = models.CharField(max_length=5)
     postal_code = models.CharField(max_length=10)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.province}/{self.city}"
 
 
 
