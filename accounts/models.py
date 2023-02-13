c133195ba4136599533fad09c984d91e893a1574
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-13 17:41:52 +0330

    028_develop: add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,36 +1,35 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
 
 # Create your models here.
-class Customer(models.Model):
-    """Example of docstring on the __init__ method.
+class User(AbstractUser):
+    """
+        Stores users information, inherited from django AbstractUser
+    """
+    phone = models.CharField(max_length=11, null=True, blank=True)
+    username = None
+    email = models.EmailField(max_length=255, unique=True)
 
-            The __init__ method may be documented in either the class level
-            docstring, or as a docstring on the __init__ method itself.
+    objects = UserManager()
 
-            Either form is acceptable, but the two should not be mixed. Choose one
-            convention to document the __init__ method and be consistent with it.
-
-            Note:
-                Do not include the `self` parameter in the ``Args`` section.
-
-            Args:
-                param1 (str): Description of `param1`.
-                param2 (:obj:`int`, optional): Description of `param2`. Multiple
-                    lines are supported.
-                param3 (:obj:`list` of :obj:`str`): Description of `param3`.
-
-            """
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone = models.CharField(max_length=10, null=True, blank=True)
-    username = models.CharField(max_length=20)
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
 
 class Address(models.Model):
@@ -51,11 +50,7 @@ class Address(models.Model):
     alley = models.CharField(max_length=50)
     num = models.CharField(max_length=5, null=True, blank=True)
     postal_code = models.CharField(max_length=10, null=True, blank=True)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.province}/{self.city}"
-
-
-
-
