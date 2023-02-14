6bfed0fcc021523d1e69594c28ed55860f7d72d6
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-14 13:46:20 +0330

    034_develop: add user registration urls and view

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls', namespace='landing'))
+    path('', include('product.urls', namespace='landing')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
