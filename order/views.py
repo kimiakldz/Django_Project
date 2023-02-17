429eec0025d80f61cd1c739ae0ea05b8f010a46c
kimiakldz <kkolahdooz2020@gmail.com>
2023-02-17 14:19:56 +0330

    051_develop: make cart class iterable

--- order/views.py
+++ order/views.py
@@ -21,10 +21,32 @@ class Cart:
             cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
 
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['total_price'] = int(item['price']) * item['quantity']
+            yield item
+
 
 class CartView(View):
     def get(self, request):
-        return render(request, 'cart.html')
+        cart = Cart(request)
+        return render(request, 'cart.html', {'cart': cart})
 
 
 class CartAddView(PermissionRequiredMixin, View):
@@ -36,4 +58,6 @@ class CartAddView(PermissionRequiredMixin, View):
         form = CartAddForm(request.POST)
         if form.is_valid():
             cart.add(product, form.cleaned_data['quantity'])
-        return redirect('orders:cart')
+        return redirect('order:cart')
+
+
